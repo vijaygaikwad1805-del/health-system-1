76f7e3f9f4139df984e60e763033eca5bd29e6b8
vijaygaikwad1805-del <vijaygaikwad1805@gmail.com>
2026-04-17 16:17:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,40 @@
 async function register() {
-  const res = await fetch("https://health-system-1-j2hi.onrender.com/register", {
-    method: "POST",
-    headers: {"Content-Type": "application/json"},
-    body: JSON.stringify({
-      name: document.getElementById("name").value,
-      email: document.getElementById("email").value,
-      password: document.getElementById("password").value
-    })
-  });
+  try {
+    const res = await fetch("https://health-system-1-j2hi.onrender.com/register", {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        password: document.getElementById("password").value
+      })
+    });
 
-  alert("Registered Successfully");
+    alert("Registered Successfully");
+  } catch (err) {
+    alert("Error: " + err);
+  }
 }
 
 async function login() {
-  const res = await fetch("https://health-system-1-j2hi.onrender.com/login", {
-    method: "POST",
-    headers: {"Content-Type": "application/json"},
-    body: JSON.stringify({
-      email: document.getElementById("email").value,
-      password: document.getElementById("password").value
-    })
-  });
+  try {
+    const res = await fetch("https://health-system-1-j2hi.onrender.com/login", {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({
+        email: document.getElementById("email").value,
+        password: document.getElementById("password").value
+      })
+    });
 
-  const data = await res.text();
+    const data = await res.text();
 
-  if(data === "Success"){
-    window.location.href = "dashboard.html";
-  } else {
-    alert("Wrong email or password");
+    if(data === "Success"){
+      window.location.href = "dashboard.html";
+    } else {
+      alert("Wrong email or password");
+    }
+  } catch (err) {
+    alert("Error: " + err);
   }
 }
